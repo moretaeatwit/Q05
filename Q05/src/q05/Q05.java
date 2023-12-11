97d1a85d0282e9c44d1299c7be07b0c45d893e79
Ewdi <Ewdi@10.0.0.91>
2023-12-11 16:13:31 -0500

    Initial Commit

---
+++ Q05/src/q05/Q05.java
@@ -0,0 +1,18 @@
+package q05;
+import java.util.Scanner;
+
+public class Q05 {
+
+	public Q05() {
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.printf("What is your favorite day of the week? ");
+		String day = input.next();
+
+	}
+
+}
